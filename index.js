39c4c07bc398416472df4043ba8d0e8aa7ab2cef
Larson Collier <collierlarson@gmail.com>
2022-10-19 13:43:16 -0400

    commit tree 8d23aaa6bc32e0d98702ccbaeee0a78fa86a157f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+/*const gifts = ["teddy","drone", "doll"]
+function wrapGifts(gifts){
+    for(let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts
+}
+
+wrapGifts(gifts)
+
+*/
+
+const name = []
+const event = []
+
+
+function writeCards(name, event){
+    let greetings = []
+    for(let i = 0; i<name.length ; i++){
+            let currentName = name[i]
+            greetings.push(`Thank you, ${currentName}, for the wonderful ${event} gift!`)
+           // debugger
+    }
+    return greetings
+}
+////////////////////////////
+let number = []
+
+function countDown(number) {
+    debugger
+
+    while(number >= 0){
+
+        console.log(number);
+        number --;
+
+    }
+
+}
